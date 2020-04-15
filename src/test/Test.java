eb995019df3849c595350201e0220b84569d9810
Jason <Jasonneeed@163.com>
2020-04-15 23:06:58 +0800

    量有点大

---
+++ src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+
+        map.put("1", 1);
+    }
+}
